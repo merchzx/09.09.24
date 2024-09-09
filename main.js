5e4c6990b96d0685ce0022907b2a454d094857aa
Artem <yabublik08@gmail.com>
2024-09-09 16:23:01 +0300

    новая функция диалог окна

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.write("Hello World!");
+document.write("My name is Artem <br>I am 16 years old<br>I like going to the gym and tasty food");
+let number, summ = 0;
+        do {
+            number = parseInt(prompt("Введите число:", 10));
+            summ += number;
+        }
+        while (confirm("Продолжим?"));
+        alert("Сумма чисел: " + summ);
